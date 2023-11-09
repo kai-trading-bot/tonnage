d7c4e8b8de5d4c1e46cd10f3e9275c71bdc00b71
statarb <kqureshi@mit.edu>
2023-11-09 15:50:58 -0800

    Update data.py

--- data.py
+++ data.py
@@ -3,23 +3,23 @@ import pandas as pd
 import ipywidgets as widgets
 import plotly.offline as py
 import plotly.graph_objs as go
+import warnings
 
 py.init_notebook_mode(connected=True)
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 __author__ = 'kqureshi'
 
-DATA_PATH = '/Users/kq/Downloads/DSNY_Monthly_Tonnage_Data.csv'
-
+DATA_PATH = '/Users/kq/Downloads/aggregated_Tonnage_byYear_cleaned.csv'
 
 def fetch_data() -> pd.DataFrame:
     """
     Fetch and process data
     """
-    data = pd.read_csv(DATA_PATH).drop('BOROUGH_ID', axis=1)
-    data['YEAR'] = data.MONTH.str.split(' / ').str[0]
-    data['MONTH'] = data.MONTH.str.split(' / ').str[1]
-    data = data.groupby(['YEAR', 'BOROUGH']).sum().reset_index()
+    data = pd.read_csv(DATA_PATH)
     data.YEAR = data.YEAR.astype(str)
+    data.DISTRICT = data.DISTRICT.astype(str)    
     return data
 
 
@@ -27,12 +27,37 @@ def update_plot(slider_year: int) -> py.iplot:
     """
     Use slider to query for year and create diagram
     """
-    data = fetch_data()
-    df = data.query('YEAR=="{year}"'.format(year=slider_year)).set_index('BOROUGH').drop('YEAR', axis=1)
+
     links = []
-    for borough in list(df.index):
-        for target in df.loc[borough].reset_index().values:
-            links.append({'source': borough, 'target': target[0], 'value': target[1]})
+    data = fetch_data()
+    data = data[data.YEAR == str(slider_year)]
+    grouper = data.groupby(['YEAR', 'BOROUGH', 'DISTRICT']).sum()
+    years = sorted(grouper.index.levels[0])
+    boroughs = sorted(grouper.index.levels[1])
+    districts = sorted(grouper.index.levels[2])
+
+    for year in years:
+        year_level = grouper.xs(year, level=0)    
+        borough_level = year_level.sum(level=0)
+        sum_borough = borough_level.sum(axis=1)
+        for borough in sorted(sum_borough.index.unique()):
+            links.append({'source': year,
+                                  'target': borough,
+                                  'value': sum_borough.loc[borough]})
+
+        for borough in sorted(borough_level.index.unique()):
+            district_level = year_level.xs(borough)
+            sum_district = district_level.sum(axis=1)
+            for district in sorted(sum_district.index):
+                links.append({'source': borough,
+                                      'target': district,
+                                      'value': sum_district.loc[district]})
+            for district in sorted(district_level.index.unique()):
+                target_district = district_level.loc[district]
+                for style in sorted(target_district.index):
+                    links.append({'source': district,
+                                  'target': style,
+                                  'value': target_district.loc[style]})
 
     df = pd.DataFrame(links)
     nodes = np.unique(df[["source", "target"]], axis=None)
@@ -47,10 +72,13 @@ def update_plot(slider_year: int) -> py.iplot:
             },
         )
     )
-    fig.update_layout(title=dict(text="{year} Collection Distribution".format(year=slider_year), font=dict(size=25)))
+    fig.update_layout(title=dict(text="{year} Collection Distribution".format(year=slider_year), font=dict(size=12)))
     py.iplot(fig, filename='HISTORICAL COLLECTION RATES')
+   
 
-
-data = fetch_data()
-years = sorted(data.YEAR.unique())
-min_year, max_year = int(years[0]), int(years[-1])
+def fetch_figure() -> None:
+    data = fetch_data()
+    years = sorted(data.YEAR.unique())
+    min_year, max_year = int(years[0]), int(years[-1])
+    widgets.interact(update_plot, slider_year=(min_year, max_year))
+    
